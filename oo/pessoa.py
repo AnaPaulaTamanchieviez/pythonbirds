272a76872d72d0fa3be9c98db69f44fc392f1d22
Ana Paula Tamanchieviez <ana-paulatamanchieviez@hotmail.com>
2021-10-07 21:50:32 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,16 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=23):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar (self):
-        return f'Olá {id(self)}'
+        return f'Olá {p.nome}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Ana')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ana'
+    print(p.cumprimentar())
+
 
